0ec29289e755bedbe63f085360db464b7e1c8c73
Ninjaondra321 <ondran321@gmail.com>
2024-06-26 19:06:23 +0200

    General updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,26 @@
 import { Routes } from '@angular/router';
-import { BmiCalculatorComponent } from './calculator/calculator.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 import { DuplicatorComponent } from './duplicator/duplicator.component';
 
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsHomepageComponent } from './products-homepage/products-homepage.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { QuoteComponent } from './quote/quote.component';
 
 
 export const routes: Routes = [
-    {path: 'calculator', component: BmiCalculatorComponent},
-    {path: 'calc', component: BmiCalculatorComponent},
+    {path: 'calculator', component: CalculatorComponent},
+    {path: 'calc', redirectTo:"calculator"},
 
     {path: 'duplicator', component: DuplicatorComponent},
-    {path: 'dup', component: DuplicatorComponent},
+    {path: 'dup', redirectTo:"duplicator"},
+    
+    {path: 'products', component: ProductsHomepageComponent},
+    {path: 'prod', redirectTo:"products"},
+    
+    {path: 'products/:productName', component: ProductDetailComponent},
+    
+    {path: 'quote', component: QuoteComponent},
 
     {path: '', component: HomepageComponent},
 ];
